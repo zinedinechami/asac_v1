467b78adf0b112439c82d46baf336eacb9454ad5
Zinedine Chami <zinedine.chami@gmail.com>
2024-04-29 23:23:43 +0200

    initialised react router and added json database file

--- asac_react/src/App.tsx
+++ asac_react/src/App.tsx
@@ -1,9 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import Home from "./pages/home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <div>Hello World</div>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
