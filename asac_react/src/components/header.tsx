734fc2d7a3b1d243d0cb19f002034bfc15c35d05
Zinedine Chami <zinedine.chami@gmail.com>
2024-05-04 21:44:01 +0200

    added home hero

--- asac_react/src/components/header.tsx
+++ asac_react/src/components/header.tsx
@@ -5,14 +5,10 @@ export default function Header() {
   //   github link
   return (
     <>
-      <nav className="bg-stone-950 h-28 w-full  flex shadow">
+      <nav className="bg-stone-950 h-24 w-full  flex shadow-xl">
         <h1 className="text-neutral-50 figtree_bold text-4xl mt-6 pl-12 italic">
           asac
         </h1>
-
-        <div className="flex mx-auto mt-6 ">
-          <input className="h-12 w-96 rounded-xl bg-neutral-900"></input>
-        </div>
       </nav>
     </>
   );
--- asac_react/src/pages/home.tsx
+++ asac_react/src/pages/home.tsx
@@ -1,21 +1,28 @@
 import Header from "../components/header";
 import database from "../database.json";
 
+// header that becomes sticky at scroll down?
+
 export default function Home() {
   return (
     <>
       <Header />
 
       <section className="bg-neutral-900 w-full ">
-        {/* <div>
+        <div className="pt-20 ">
           <h1 className="text-center text-5xl font-bold text-neutral-50">
             A Home Away from Home
           </h1>
-          <p>Spend the cheapest nights at the best appartements </p>
-          <button className="mx-auto">lol</button>
-        </div> */}
+          <p className="text-center pt-5 text-neutral-50">
+            Find the cheapest nights at the best appartements{" "}
+          </p>
+          {/* <button className="mx-auto">lol</button> */}
+          <div className="mt-8  text-center">
+            <input className="h-12 w-96 rounded-full bg-neutral-50 mx-auto "></input>
+          </div>
+        </div>
 
-        <div className="mx-auto flex flex-wrap w-11/12">
+        <div className="ml-auto flex flex-wrap w-11/12 pt-20">
           {database.map((data) => {
             return (
               <a key={data.id} href={"logement/" + data.id}>
