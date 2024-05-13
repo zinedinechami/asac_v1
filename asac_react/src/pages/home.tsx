e07bd9b45e7a13060375fa9c297afd4c122f3318
Zinedine Chami <zinedine.chami@gmail.com>
2024-05-14 00:43:51 +0200

    new visual light identity

--- asac_react/src/pages/home.tsx
+++ asac_react/src/pages/home.tsx
@@ -1,5 +1,5 @@
 import database from "../database.json";
-import github_logo from "../assets/pngegg.png";
+
 import { useState } from "react";
 
 // header that becomes sticky at scroll down?
@@ -13,116 +13,131 @@ export default function Home() {
 
   return (
     <>
-      <section className=" w-full      bg-gradient-to-l from-neutral-900 to-black ">
-        <h1 className="text-neutral-50 figtree_bold text-4xl pt-5 pl-12 italic ">
-          asac
-        </h1>
-        <a
-          className="hover:opacity-80 transition-opacity"
-          href="https://github.com/zinedinechami/asac_v1"
-        >
-          <img
-            src={github_logo}
-            alt="github_logo"
-            className="w-10 absolute top-6 right-12"
-          />
-        </a>
-        <div className="pt-24 pb-12 border-solid border-0 border-b border-neutral-800">
-          <h1 className="text-center text-6xl  font-bold text-neutral-50 ">
-            Your Home, Everywhere
-          </h1>
-          <p className="text-center pt-2 text-neutral-200">
-            Find the cheapest nights at the best appartements{" "}
-          </p>
-          {/* <button className="mx-auto">lol</button> */}
-          <div className="mt-8 mb-2 text-center">
-            <input
-              onChange={(e) => setSearch(e.target.value)}
-              placeholder="Search asac.com"
-              className="h-12 w-80 rounded-lg cursor-pointer  bg-neutral-50 mx-auto   text-center focus:ring-0 outline-0  transition-colors"
-            ></input>
-            <a href="#appartements">
-              <button className="h-12  bg-emerald-500 hover:bg-emerald-600 transition-colors text-neutral-50 mx-4 px-4 rounded-lg font-semibold">
-                Search
-              </button>
-            </a>
+      <section className=" w-full  bg-neutral-50 ">
+        <div className="bg-gradient-to-t bg-neutral-100">
+          <a className="hover:opacity-80 transition-opacity" href="/">
+            <h1 className="text-neutral-900 font-semibold text-4xl pt-6 pl-12  ">
+              Asac
+            </h1>
+          </a>
+          <a
+            className="hover:opacity-80 transition-opacity"
+            href="https://github.com/zinedinechami/asac_v1"
+          >
+            {/* <img
+              src={github_logo}
+              alt="github_logo"
+              className="w-10 absolute top-6 right-12"
+            /> */}
+            <svg
+              className="w-10 absolute top-6 right-12"
+              xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+            </svg>
+          </a>
+          <div className="pt-16 pb-8 border-solid  ">
+            <h1 className="text-center text-6xl  font-bold text-neutral-950 ">
+              Your Home, Everywhere
+            </h1>
+            <p className="text-center pt-2 text-neutral-500 ">
+              Find the cheapest nights at the best appartements{" "}
+            </p>
+            {/* <button className="mx-auto">lol</button> */}
+            <div className="mt-6 mb-2 text-center">
+              <input
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder="Search asac.com"
+                className="h-12 w-80 rounded-full cursor-pointer shadow-sm  bg-white mx-auto  border text-center focus:ring-0 outline-0  transition-colors"
+              ></input>
+              <a href="#appartements">
+                <button className="h-12  bg-neutral-900 hover:bg-neutral-700   transition-colors text-neutral-50 mx-4 px-5 rounded-full font-semibold">
+                  Search
+                </button>
+              </a>
+            </div>
           </div>
-          <h2 className="text-center text-neutral-50 pt-6">
-            Join our community !
-          </h2>
-          <div className="flex -space-x-4 rtl:space-x-reverse justify-center pt-2">
+
+          {/* <div className="flex -space-x-4 rtl:space-x-reverse justify-center pt-8">
             <img
-              className="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800"
+              className="w-10 h-10 border-2  rounded-full border-neutral-300"
               src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/profile-picture-1.jpg"
               alt=""
             />
             <img
-              className="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800"
+              className="w-10 h-10 border-2  rounded-full border-neutral-300"
               src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/profile-picture-2.jpg"
               alt=""
             />
             <img
-              className="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800"
+              className="w-10 h-10 border-2  rounded-full border-neutral-300"
               src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/profile-picture-3.jpg"
               alt=""
             />
             <img
-              className="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800"
+              className="w-10 h-10 border-2  rounded-full border-neutral-300"
               src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/profile-picture-4.jpg"
               alt=""
             />
-            <div className="flex items-center justify-center w-10 h-10 text-xs font-medium text-white bg-neutral-500 border-2 border-neutral-800 rounded-full  ">
+            <div className="flex items-center justify-center w-10 h-10 text-xs font-medium text-black bg-neutral-50 border-2 border-neutral-300 rounded-full  ">
               +1k
             </div>
           </div>
+          <h2 className="text-center text-neutral-950 font-semibold pt-2">
+            Join our community !
+          </h2> */}
         </div>
-
-        <h1 className="text-neutral-50 pl-48 pt-10 text-xl">
-          Over {database.length} Appartements in{" "}
-          <span className="font-bold">Paris</span>{" "}
-        </h1>
-        <div
-          id="appartements"
-          className="ml-auto flex flex-wrap w-11/12 pt-10 pl-16"
-        >
-          {/* <nav className="bg-stone-950 h-16 w-full sticky top-0 flex  border-solid border-0 border-b border-neutral-800 z-50">
+        <div className="border rounded-xl">
+          <h1 className="text-neutral-900 pl-48 pt-10 text-base">
+            Over {database.length} Appartements in{" "}
+            <span className="font-bold">Paris</span>{" "}
+          </h1>
+          <div
+            id="appartements"
+            className="ml-auto flex flex-wrap w-11/12 pt-10 pl-16"
+          >
+            {/* <nav className="bg-stone-950 h-16 w-full sticky top-0 flex  border-solid border-0 border-b border-neutral-800 z-50">
             <h1 className="text-neutral-50 figtree_bold text-3xl mt-3 pl-12 italic ">
               asac
             </h1>
           </nav> */}
-          {database
-            .filter((data) => {
-              return search.toLowerCase() === ""
-                ? data
-                : data.title.toLowerCase().includes(search);
-            })
-            .map((data) => {
-              return (
-                <a key={data.id} href={"logement/" + data.id}>
-                  <article className="w-64 h-64 mr-10 mb-20 relative rounded-xl   opacity-90 ">
-                    <div className="hover:opacity-80 transition-opacity ">
-                      <img
-                        className="w-72 h-60 object-cover rounded-lg "
-                        src={data.cover}
-                        alt=""
-                      />
-                    </div>
-                    <div className="mb-2">
-                      <p className="text-neutral-50 pt-2 text-sm font-bold">
-                        {data.title}
-                      </p>
-                      <p className="text-neutral-200 text-sm ">
-                        Hosted by {data.host.name}
-                      </p>
-                      <p className="text-neutral-200 text-sm font-bold ">
-                        $ {data.price}{" "}
-                        <span className="font-normal">night</span>
-                      </p>
-                    </div>
-                  </article>
-                </a>
-              );
-            })}
+            {database
+              .filter((data) => {
+                return search.toLowerCase() === ""
+                  ? data
+                  : data.title.toLowerCase().includes(search);
+              })
+              .map((data) => {
+                return (
+                  <a key={data.id} href={"logement/" + data.id}>
+                    <article className="w-64 h-64 mr-10 mb-20 relative rounded-xl    ">
+                      <div className="hover:opacity-80 transition-opacity ">
+                        <img
+                          className="w-72 h-60 object-cover rounded-lg "
+                          src={data.cover}
+                          alt=""
+                        />
+                      </div>
+                      <div className="mb-2">
+                        <p className="text-neutral-900 pt-2 text-sm font-bold">
+                          {data.title}
+                        </p>
+                        <p className="text-neutral-900 text-sm ">
+                          Hosted by {data.host.name}
+                        </p>
+                        <p className="text-neutral-900 text-sm font-bold ">
+                          $ {data.price}{" "}
+                          <span className="font-normal">night</span>
+                        </p>
+                      </div>
+                    </article>
+                  </a>
+                );
+              })}
+          </div>
         </div>
       </section>
     </>
--- asac_react/src/pages/logement.tsx
+++ asac_react/src/pages/logement.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 import database from "../database.json";
 import Carrousel from "../components/carrousel";
-import Github from "../assets/pngegg.png";
+
 // import Header from "../components/header";
 // import Home from "./home";
 
@@ -36,11 +36,11 @@ export default function Logement() {
       {/* {data === undefined && <Home />} */}
 
       {data && (
-        <section className="bg-gradient-to-l from-neutral-900 to-black  text-neutral-50 pb-96">
-          <nav className="h-20 border-solid border-0 border-b border-neutral-800">
+        <section className="bg-neutral-50  text-neutral-950 pb-96">
+          <nav className="h-24 border-solid  border-b bg-neutral-100 border">
             <a className="hover:opacity-80 transition-opacity" href="/">
-              <h1 className="text-neutral-100 figtree_bold text-4xl pt-5 pl-12 italic ">
-                asac
+              <h1 className="text-neutral-900 font-semibold text-4xl pt-6 pl-12  ">
+                Asac
               </h1>
             </a>
 
@@ -48,11 +48,15 @@ export default function Logement() {
               className="hover:opacity-80 transition-opacity"
               href="https://github.com/zinedinechami/asac_v1"
             >
-              <img
-                src={Github}
-                alt="github_logo"
+              <svg
                 className="w-10 absolute top-6 right-12"
-              />
+                xmlns="http://www.w3.org/2000/svg"
+                width="50"
+                height="50"
+                viewBox="0 0 24 24"
+              >
+                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+              </svg>
             </a>
           </nav>
           <div className="w-8/12 mx-auto pt-12">
@@ -63,10 +67,13 @@ export default function Logement() {
                 <h2>{data?.location}</h2>
                 <h3>Hosted by {data?.host.name}</h3>
               </div>
-              <div className=" right-0 top-8 absolute  w-96 py-6 bg-neutral-900 p-4 mb-4 rounded-xl border border-neutral-700 ">
-                <h1 className="text-2xl font-bold mb-2">137£ night</h1>
-                <h2 className="text-2xl">{data?.rating}</h2>
-                <button className="w-full py-2 bg-slate-50 text-neutral-950 rounded-xl font-bold hover:bg-slate-200 transition-colors">
+              <div className=" right-0 top-8 absolute  w-96 py-6 bg-neutral-100 p-4 mb-4 rounded-xl border shadow-sm  ">
+                <h1 className="text-2xl font-bold mb-2">
+                  {" "}
+                  $ {data?.price} night
+                </h1>
+                {/* <h2 className="text-2xl">{data?.rating}</h2> */}
+                <button className="w-full py-3 bg-neutral-950 text-neutral-50 rounded-xl font-bold hover:bg-neutral-800 transition-colors">
                   Reserve
                 </button>
               </div>
